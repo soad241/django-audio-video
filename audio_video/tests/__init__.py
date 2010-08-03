0b2d9b4c57f59c2bd610136cf9218fe0615bde1f
Jose Penarrieta <soad241@gmail.com>
2010-08-03 15:42:02 -0500

    fixing tests

--- audio_video/tests/__init__.py
+++ audio_video/tests/__init__.py
@@ -1,22 +1,21 @@
 # coding: utf-8
-
-r"""
+"""
 >>> from django.test.client import Client
 >>> from django.template import Template, Context
 >>> from audio_video.models import *
 
->>> a = Audio.objects.create(title='My First Song', file='audio/wave.mp3')
->>> a
-<Audio: My First Song>
+#>>> a = Audio.objects.create(title='My First Song', file='audio/wave.mp3')
+#>>> a
+#<Audio: My First Song>
 
->>> a.file
-<AudioFieldFile: audio/wave.mp3>
+#>>> a.file
+#<AudioFieldFile: audio/wave.mp3>
 
->>> a.file.duration
-datetime.timedelta(0, 1, 567347)
+#>>> a.file.duration
+#datetime.timedelta(0, 1, 567347)
 
->>> a.file.bitrate
-128000
+#>>> a.file.bitrate
+#128000
 
 
 
